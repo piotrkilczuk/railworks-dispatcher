f6a6e04cf27d27fbc223867eaf2d33df11950025
Piotr Kilczuk <piotr@tymaszweb.pl>
2015-01-06 11:00:50 +0000

    Allow to create multiple work order files

--- dispatcher.py
+++ dispatcher.py
@@ -87,7 +87,7 @@ def render_html(context):
 
 
 def main():
-    railworks_folder = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
+    railworks_folder = os.getcwd()
     scenario_folders = os.path.join(railworks_folder,
                                     'Content', 'Routes', '*', 'Scenarios', '*', 'ScenarioProperties.xml')
     work_orders_folder = os.path.join(railworks_folder, 'WorkOrders')
@@ -95,63 +95,65 @@ def main():
     dispatcher_folder = os.path.abspath(os.path.dirname(sys.argv[0]))
     dispatcher_data_folder = os.path.join(dispatcher_folder, 'Dispatcher')
 
+    order_count = int(sys.argv[1]) if len(sys.argv) > 1 else 1
     all_scenarios = glob.glob(scenario_folders)
 
-    while True:
-        todays_work_order = random.choice(all_scenarios)
+    for o in range(order_count):
+        while True:
+            todays_work_order = random.choice(all_scenarios)
 
-        # only allow scenarios from existing unpacked routes
-        scenario_folder_suffix = os.path.join(*todays_work_order.split(os.sep)[-3:])
-        route_folder = todays_work_order.replace(scenario_folder_suffix, '')
-        route_description = os.path.join(route_folder, 'RouteProperties.xml')
-        if not os.path.exists(route_description):
-            continue
+            # only allow scenarios from existing unpacked routes
+            scenario_folder_suffix = os.path.join(*todays_work_order.split(os.sep)[-3:])
+            route_folder = todays_work_order.replace(scenario_folder_suffix, '')
+            route_description = os.path.join(route_folder, 'RouteProperties.xml')
+            if not os.path.exists(route_description):
+                continue
 
-        xml = ElementTree.parse(todays_work_order)
+            xml = ElementTree.parse(todays_work_order)
 
-        scenario_class = xml.find('./ScenarioClass').text
-        if scenario_class in IGNORED_SCENARIO_CLASSES:
-            continue
+            scenario_class = xml.find('./ScenarioClass').text
+            if scenario_class in IGNORED_SCENARIO_CLASSES:
+                continue
 
-        scenario_name = xml.find('./DisplayName/Localisation-cUserLocalisedString/English').text
-        scenario_description = xml.find('./Description/Localisation-cUserLocalisedString/English').text
-        scenario_briefing = xml.find('./Briefing/Localisation-cUserLocalisedString/English').text
-        scenario_start_location = xml.find('./StartLocation/Localisation-cUserLocalisedString/English').text
-        scenario_start_time = xml.find('./StartTime').text
-        scenario_start_day = xml.find('./StartDD').text
-        scenario_start_month = xml.find('./StartMM').text
-        scenario_start_year = xml.find('./StartYYYY').text
+            scenario_name = xml.find('./DisplayName/Localisation-cUserLocalisedString/English').text
+            scenario_description = xml.find('./Description/Localisation-cUserLocalisedString/English').text
+            scenario_briefing = xml.find('./Briefing/Localisation-cUserLocalisedString/English').text
+            scenario_start_location = xml.find('./StartLocation/Localisation-cUserLocalisedString/English').text
+            scenario_start_time = xml.find('./StartTime').text
+            scenario_start_day = xml.find('./StartDD').text
+            scenario_start_month = xml.find('./StartMM').text
+            scenario_start_year = xml.find('./StartYYYY').text
 
-        time = int_to_time(int(scenario_start_time))
-        time_adjust = random.randrange(15, 45)
-        date = datetime.datetime(
-            int(scenario_start_year), int(scenario_start_month), int(scenario_start_day),
-            *time
-        ) - datetime.timedelta(minutes=time_adjust)
+            time = int_to_time(int(scenario_start_time))
+            time_adjust = random.randrange(15, 45)
+            date = datetime.datetime(
+                int(scenario_start_year), int(scenario_start_month), int(scenario_start_day),
+                *time
+            ) - datetime.timedelta(minutes=time_adjust)
 
-        break
+            break
 
-    work_order_number = str(get_last_number(work_orders_folder) + 1).zfill(4)
+        work_order_number = str(get_last_number(work_orders_folder) + 1).zfill(4)
 
-    ensure_folder(work_orders_folder)
+        ensure_folder(work_orders_folder)
 
-    html = render_html({
-        'date': date,
-        'dispatcher_artwork_folder': os.path.join(dispatcher_data_folder, 'Artwork'),
-        'logo': 'BR.jpg',
-        'scenario_name': scenario_name,
-        'scenario_description': scenario_description,
-        'scenario_briefing': scenario_briefing,
-        'scenario_start_location': scenario_start_location or 'Depot',
-        'scenario_class': scenario_class,
-        'shift_number': work_order_number,
-        'username': humanize_username(getpass.getuser()),
-    })
+        html = render_html({
+            'date': date,
+            'dispatcher_artwork_folder': os.path.join(dispatcher_data_folder, 'Artwork'),
+            'logo': 'BR.jpg',
+            'scenario_name': scenario_name,
+            'scenario_description': scenario_description,
+            'scenario_briefing': scenario_briefing,
+            'scenario_start_location': scenario_start_location or 'Depot',
+            'scenario_class': scenario_class,
+            'shift_number': work_order_number,
+            'username': humanize_username(getpass.getuser()),
+        })
 
-    html_name = work_order_number + '.html'
-    html_path = os.path.join(work_orders_folder, html_name)
-    open(html_path, 'w').write(html.encode('utf8'))
-    launch_html(html_path)
+        html_name = work_order_number + '.html'
+        html_path = os.path.join(work_orders_folder, html_name)
+        open(html_path, 'w').write(html.encode('utf8'))
+        launch_html(html_path)
 
 
 if __name__ == '__main__':
