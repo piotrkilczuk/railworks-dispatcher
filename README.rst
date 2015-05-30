52d66a2315ef9b6108a1beacac3ef9dcf09af718
Piotr Kilczuk <piotr@tymaszweb.pl>
2015-05-30 23:49:59 +0100

    Initial commit on Windows branch
    Hooray!

---
+++ README.rst
@@ -0,0 +1,8 @@
+Developing
+----------
+
+In order to develop on Windows you'll need:
+
+* Python 3 (recommended 3.4)
+* git
+* pywin32
---
+++ dispatcher.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+import _winreg
+
+
+steam_key = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, 'Software\\Valve\\Steam')
+steam_path = _winreg.QueryValueEx(steam_key, 'SteamPath')[0]
+railworks_path = os.path.join(steam_path, 'steamApps', 'common', 'railworks')
+railworks_exe = os.path.join(railworks_path, 'railworks.exe')
+
+if os.path.isfile(os.path.join(railworks_path, 'railworks.exe')):
+	raw_input(os.path.abspath(railworks_exe))
+
+
